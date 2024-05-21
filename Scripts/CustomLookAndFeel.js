d98a7e8a161f7da533c33f7a857d5fcc0e4e01a5
clinthof <73394309+clinthof@users.noreply.github.com>
2024-05-20 22:24:57 -0400

    Add simple XY panel hooked up to LFO mods

--- Scripts/CustomLookAndFeel.js
+++ Scripts/CustomLookAndFeel.js
@@ -11,4 +11,17 @@ namespace CustomLookAndFeel
 		g.setColour(obj.value ? obj.bgColour : obj.itemColour1);
 		g.fillRoundedRectangle(obj.area, 10);
 	});
+	
+	laf.registerFunction("drawAlertWindow", function(g, obj)
+	{
+	    g.fillAll(0xFF333333);
+	    
+	    var a = [0, 0, obj.area[2], 30];
+	    g.setColour(0x44000000);
+	    g.fillRect(a);
+	    g.setColour(Colours.white);
+	    g.drawRect(obj.area, 0.3);
+	    g.setFont("Comic Sans MS", 18);
+	    g.drawAlignedText(obj.title, a, "centred");
+	});
 }
